5469f339ecd55f17634648afb15b59b09e086ccd
sandyhem <santhoshkowsalya2004@gmail.com>
2024-07-25 19:20:25 +0530

    created the routes for the navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import About from './pages/About'
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+       <Routes>
+         <Route path="/" element={<Home/>}></Route>
+         <Route path="/profile" element={<Profile/>}></Route>
+         <Route path="/signin" element={<Signin/>}></Route>
+         <Route path="/signup" element={<Signup/>}></Route>
+         <Route path="/about" element={<About/>}></Route>
+       </Routes>
+    </BrowserRouter>
   )
 }
